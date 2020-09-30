156be8b3e3ee05a2ffca54bc1cb1aa727efdcf19
Kirtiraj <kirtirajg@prdxn.com>
2020-09-30 19:39:41 +0530

    login: static user data and addEventlistener on form element in script added

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,9 +2,19 @@
 
 */
 
+// Static User Credentials
+var userData = [
+	{'username': 'test',
+	 'password': 'test123'
+	}
+]
 
-
-
+// add event listerner on form and pass login()
+var form = document.querySelector('form');
+form.addEventListener('submit', function(e){
+	e.preventDefault();
+	login();
+});
 
 
 
