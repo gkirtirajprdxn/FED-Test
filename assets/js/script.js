bf5c59d3dabd74db30841ab79f3b72b268521867
Kirtiraj <kirtirajg@prdxn.com>
2020-09-30 19:42:16 +0530

    login: login funnction in script added

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,7 +16,22 @@ form.addEventListener('submit', function(e){
 	login();
 });
 
+var user = document.getElementById('username');
+var pass = document.getElementById('password');
+var error = document.getElementById('errormsg');
 
+// user login function
+function login(){
+	for (var i=0; i<userData.length; i++) {
+		if ((user.value == userData[i].username) && (pass.value == userData[i].password)) {
+			window.location.href = 'index.html';
+			return true;
+		} else {
+			error.style.display = 'block';
+			return false;
+		};
+	};
+};
 
 
 
