7ee6ffbb8547dc94fb342e02e6705506b38d6eb6
Kirtiraj <kirtirajg@prdxn.com>
2020-10-04 14:47:06 +0530

    login-js: localstorage setItems added

--- assets/js/script.js
+++ assets/js/script.js
@@ -26,6 +26,9 @@ var error = document.getElementById('errormsg');
 function login(){
 	for (var i=0; i<userData.length; i++) {
 		if ((user.value == userData[i].username) && (pass.value == userData[i].password)) {
+			// set username and password into local Storage
+			localStorage.setItem(user.value, pass.value) 
+			// redirect to index.html
 			window.location.href = 'index.html';
 			return true;
 		} else {
